7a2917e33338ce0efad9bf733fda71f6f0364887
Julien Cambier <julien@Juliens-MacBook-Pro.local>
2017-03-11 18:20:27 +0100

    0.1 sec delay + no kill between same-level videos

--- commands/ruimte/4.sh
+++ commands/ruimte/4.sh
@@ -1,5 +1,5 @@
 killall omxplayer.bin
-sleep 0.5
+sleep 0.1
 killall omxplayer.bin
 killall omxplayer.bin
 omxplayer --loop --no-osd /home/pi/App/usbdrv/assets/videos/ruimte/4.mp4
--- commands/ruimte/5-6.sh
+++ commands/ruimte/5-6.sh
@@ -1,8 +1,6 @@
 killall omxplayer.bin
-sleep 0.5
+sleep 0.1
 killall omxplayer.bin
 killall omxplayer.bin
 omxplayer --no-osd /home/pi/App/usbdrv/assets/videos/ruimte/5-6.mp4
-killall omxplayer.bin
-killall omxplayer.bin
 omxplayer --loop --no-osd /home/pi/App/usbdrv/assets/videos/ruimte/6.mp4
