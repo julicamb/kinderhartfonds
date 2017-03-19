9c0c0a468ae1ae7fe7b91890d31aed5c0a10f0a0
Julien Cambier <julien@Juliens-MacBook-Pro.local>
2017-03-19 20:44:56 +0100

    no message

--- commands/ruimte/1.sh
+++ commands/ruimte/1.sh
@@ -2,4 +2,5 @@ killall omxplayer.bin
 sleep 0.1
 killall omxplayer.bin
 killall omxplayer.bin
-omxplayer --win "240 0 1680 810" --loop --no-osd /home/pi/App/usbdrv/assets/videos/ruimte/1.mp4
+#240 0 1680 810
+omxplayer --win "0 0 1920 810" --loop --no-osd /home/pi/App/usbdrv/assets/videos/ruimte/1.mp4
