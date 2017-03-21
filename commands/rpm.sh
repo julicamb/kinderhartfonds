6e5e4227a84e40b0addaf62d8ef5633746046eff
Julien Cambier <julien@Juliens-MacBook-Pro.local>
2017-03-21 22:21:10 +0100

    no message

--- commands/rpm.sh
+++ commands/rpm.sh
@@ -1,1 +1,1 @@
-chromium-browser --window-size=400,270 --window-position=760,2000 /home/pi/App/bar_rpm/index.html
+chromium-browser --window-size=1920,150 --window-position=0,2000 -app=home/pi/App/bar_rpm/index.html
