4e1abb23bbf410ca7a15e4f9c0424a21c441e47c
Julien Cambier <julien@Juliens-MacBook-Pro.local>
2017-03-21 22:49:51 +0100

    no message

--- commands/rpm.sh
+++ commands/rpm.sh
@@ -1,1 +1,1 @@
-chromium-browser --window-size=1920,150 --window-position=0,2000 --disable-web-security --allow-file-access-from-files --allow-file-access --app=file://home/pi/App/bar_rpm/index.html
+chromium-browser --window-size=1920,150 --window-position=0,2000 --app=file://home/pi/App/bar_rpm/index.html --disable-web-security --allow-file-access-from-files --allow-file-access 
