d570dbe22e075f1a521aafa63e6829e6740ba2af
Julien Cambier <julien@Juliens-MacBook-Pro.local>
2017-03-21 21:14:18 +0100

    no message

--- commands/video.sh
+++ commands/video.sh
@@ -3,7 +3,7 @@ killall omxplayer.bin
 sleep 0.1
 killall omxplayer.bin
 killall omxplayer.bin
-if [ "$3" == "" ] ; then
+if [ $3 == "" ] ; then
     echo omxplayer --win "0 0 1920 930" --loop --no-osd /home/pi/App/usbdrv/assets/videos/$1/$2.mp4
 else
 	echo omxplayer --win "0 0 1920 930" --no-osd /home/pi/App/usbdrv/assets/videos/$1/$2-$3.mp4
